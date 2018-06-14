dc6240d03ebdcef8ced6c0bcc82aa5e49c774c88
barryntklc <barryn.chun@gmail.com>
2018-06-14 03:38:14 -1000

    initial commit 2

---
+++ win/check.win/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace check.win
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("check.win 1.0");
+            foreach (String s in args)
+            {
+                Console.WriteLine(s);
+            }
+
+            //get path
+            //
+            //return filepath
+            //return filesize
+            //return checksum
+        }
+    }
+}
