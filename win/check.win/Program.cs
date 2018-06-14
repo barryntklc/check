b7ccf7c82fa167102239742ab4b692c1501525a2
barryntklc <barryn.chun@gmail.com>
2018-06-14 09:35:58 -1000

    added -p arg handler

--- win/check.win/Program.cs
+++ win/check.win/Program.cs
@@ -11,9 +11,16 @@ namespace check.win
         static void Main(string[] args)
         {
             Console.WriteLine("check.win 1.0");
-            foreach (String s in args)
+            //foreach (String s in args)
+            //{
+            //    Console.WriteLine(s);
+            //}
+
+            if (args.Contains<String>("-p"))
             {
-                Console.WriteLine(s);
+                int pos = Array.IndexOf(args, "-p");
+                Console.WriteLine(args[pos + 1]);
+                Console.WriteLine("Reading path ");
             }
 
             //get path
